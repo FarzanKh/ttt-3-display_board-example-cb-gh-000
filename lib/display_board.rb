9eee39d5fb18993e67c079155de509a85320da28
Farzan Khoobchehr <farzan_khoobchehr@yahoo.com>
2019-05-24 03:15:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   3.times do
-    puts "   |   |   "
+    puts "   "
   end
+  
+  3.times do
+    puts "|   |"
   
   2.times do
     puts "-----------"
